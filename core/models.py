776cdcc31c87f727a8ac182854aa8c5f80a8e597
KING143ND <nvnaveenchaudhary@gmail.com>
2025-01-22 23:42:29 +0530

    Introduce profile

--- core/models.py
+++ core/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from mutagen.mp3 import MP3
       
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    favorites = models.ManyToManyField('Track', related_name='favorited_by', blank=True)
+
+    def __str__(self):
+        return str(self.first_name) + ' ' + str(self.last_name)
+    
 
 class MusicDirector(models.Model):
     name = models.CharField(max_length=255)
@@ -52,9 +64,24 @@ class Track(models.Model):
     artist = models.ManyToManyField(Singer, related_name='track_singers')
     audio_file = models.FileField(upload_to='songs/audio/', blank=True, null=True)
     category = models.CharField(max_length=50, default="")
+    duration = models.CharField(max_length=8, blank=True, null=True)
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if self.audio_file:
+            try:
+                audio = MP3(self.audio_file)
+                duration_seconds = int(audio.info.length)
+                minutes = duration_seconds // 60
+                seconds = duration_seconds % 60
+                self.duration = f"{minutes:02}:{seconds:02}"
+            except Exception as e:
+                print(f"Error extracting duration: {e}")
+        else:
+            self.duration = None
+        super().save(*args, **kwargs)
 
 
 class Playlist(models.Model):
